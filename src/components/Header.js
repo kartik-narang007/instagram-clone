b893d3e4b8ad91aff0eb06de0f3eccdfdee3e596
Kartik Narang <narangk007@gmail.com>
2024-04-26 14:16:17 +0530

    Build Left Section of Header

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl">
+
+        {/* Left */}
+
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle */}
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
