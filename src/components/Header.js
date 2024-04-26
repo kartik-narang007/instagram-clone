e5515ee73d6e01c4557a0da3bb1b2726e5b44f4a
Kartik Narang <narangk007@gmail.com>
2024-04-26 19:16:10 +0530

    Middle Section of Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 
-const Header = () => {
+function Header() {
   return (
-    <div>
-      <div className="flex justify-between max-w-6xl">
-
-        {/* Left */}
-
-        <div className="relative hidden lg:inline-grid h-24 w-24">
+    <div className="">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+        {/** Left */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -15,7 +21,7 @@ const Header = () => {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -23,12 +29,28 @@ const Header = () => {
           />
         </div>
 
-        {/* Middle */}
+        {/** Middle - Search input field */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div
+              className="absolute inset-y-0 pl-3 flex items-center
+          pointer-events-none"
+            >
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300
+            focus:ring-black rounded-md focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
 
-
+        {/** Right */}
       </div>
     </div>
   );
-};
+}
 
 export default Header;
