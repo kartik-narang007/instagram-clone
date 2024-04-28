8d6dfad31420e263d3ee523775a23f0490c39695
Kartik Narang <narangk007@gmail.com>
2024-04-28 14:22:35 +0530

    Made MiniProfile Dynamic

--- src/components/MiniProfile.js
+++ src/components/MiniProfile.js
@@ -1,12 +1,18 @@
 import { myImage } from "@/utils/constants";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const MiniProfile = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
-      <img src={myImage} className="w-16 h-16 rounded-full border p-[2px]" />
+      <img
+        src={session?.user?.image}
+        className="w-16 h-16 rounded-full border p-[2px]"
+      />
       <div className="flex-1 mx-4 cursor-pointer">
-        <h2 className="font-bold">kartik_narang_007</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
     </div>
