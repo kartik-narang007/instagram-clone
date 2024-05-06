3f68fe266992d2c42b6afd769b2258d48edd1485
Kartik Narang <narangk007@gmail.com>
2024-05-06 10:17:01 +0530

    Bug fixed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,56 +1,94 @@
 import {
-    BookmarkIcon,
-    ChatIcon,
-    DotsHorizontalIcon,
-    EmojiHappyIcon,
-    HeartIcon,
-    PaperAirplaneIcon,
-  } from "@heroicons/react/outline";
-  import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
-  
-  function Post({ id, userName, userImg, img, caption }) {
-    return (
-      <div
-        className="bg-white my-7 border
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { db } from "../../firebase";
+
+function Post({ id, userName, userImg, img, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timeStamp: serverTimestamp(),
+    });
+  };
+
+  return (
+    <div
+      className="bg-white my-7 border
       rounded-sm"
-      >
-        {/** Header */}
-        <div className="flex items-center p-5">
-          <img
-            className="h-12 w-12 rounded-full object-contain border p-1 mr-3"
-            src={userImg}
-            alt=""
-          />
-          <p className="flex-1 font-bold">{userName}</p>
-          <DotsHorizontalIcon className="h-5" />
-        </div>
-  
-        {/** Image */}
-        <img src={img} alt="" className="object-cover w-full"/>
-  
-        {/** Buttons */}
-
-        <div className="flex justify-between px-4 pt-4">
-          <div className="flex space-x-4">
-            <HeartIcon className="btn"/>
-            <ChatIcon className="btn"/>
-            <PaperAirplaneIcon className="btn"/>
-          </div>
-          <BookmarkIcon className="btn"/>
-        </div>
-
-        {/** Caption */}
-
-        <p className="p-5 truncate"><span className="font-bold mr-2">{userName}</span>{caption}</p>
-  
-        {/** Comments */}
-          <form className="flex items-center p-4">
-            <EmojiHappyIcon className="btn"/>
-            <input type="text" placeholder="Add a comment..." className="border-none flex-1 focus:ring-0 outline-none"/>
-          </form>
-        {/** Input box */}
+    >
+      {/** Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 w-12 rounded-full object-contain border p-1 mr-3"
+          src={userImg}
+          alt=""
+        />
+        <p className="flex-1 font-bold">{userName}</p>
+        <DotsHorizontalIcon className="h-5" />
       </div>
-    );
-  }
-  
-  export default Post;
+
+      {/** Image */}
+      <img src={img} alt="" className="object-cover w-full" />
+
+      {/** Buttons */}
+
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
+      {/** Caption */}
+
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{userName}</span>
+        {caption}
+      </p>
+
+      {/** Comments */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="btn" />
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button
+          type="submit"
+          disabled={!comment.trim()}
+          onClick={sendComment}
+          className="font-semibold text-blue-400"
+        >
+          Post
+        </button>
+      </form>
+      {/** Input box */}
+    </div>
+  );
+}
+
+export default Post;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,15 +4,6 @@ import { useState } from "react";
 import { db } from "../../firebase";
 import Post from "./Post";
 import { myImage } from "@/utils/constants";
-const posts = [
-  {
-    id: "1",
-    userName: "kartik_narang_007",
-    userImg: myImage,
-    img: "https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=600",
-    caption: "Hello. My first post",
-  },
-];
 
 function Posts() {
   const [posts, setPosts] = useState([]);
